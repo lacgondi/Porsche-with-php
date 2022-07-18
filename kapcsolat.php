06e1bf36805129477295e1b2968761b3fad80cc1
László Gondi <laszlo.zsolt.gondi@gmail.com>
2022-07-18 20:59:20 +0200

    Added empty check for incoming data

--- kapcsolat.php
+++ kapcsolat.php
@@ -1,5 +1,54 @@
 <?php include('inc/header.php') ?>
 
+<?php
+$first_name = $last_name = $cell_number = $email = $car = $name_on_card = $card_number = $ccv = '';
+$first_name_err = $last_name_err = $cell_number_err = $email_err = $car_err = $name_on_card_err = $card_number_err = $ccv_err = '';
+
+
+if (isset($_POST['submit'])) {
+  if (empty($first_name)) {
+    $first_name_err = "First name is required";
+  } else {
+    $first_name = filter_input(INPUT_POST, $_POST['first_name'], FILTER_SANITIZE_SPECIAL_CHARS);
+  }
+  if (empty($last_name)) {
+    $last_name_err = "Last name is required";
+  } else {
+    $last_name = filter_input(INPUT_POST, $_POST['last_name'], FILTER_SANITIZE_SPECIAL_CHARS);
+  }
+  if (empty($cell_number)) {
+    $cell_number_err = "Cell number is required";
+  } else {
+    $cell_number = filter_input(INPUT_POST, $_POST['cell_number'], FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+  }
+  if (empty($email)) {
+    $email_err = "Email is required";
+  } else {
+    $email = filter_input(INPUT_POST, $_POST['email'], FILTER_SANITIZE_EMAIL);
+  }
+  if (empty($car)) {
+    $car_err = "Please choose a car";
+  } else {
+    $car = filter_input(INPUT_POST, $car, FILTER_SANITIZE_SPECIAL_CHARS);
+  }
+  if (empty($name_on_card)) {
+    $name_on_card_err = "Name on card is required";
+  } else {
+    $name_on_card = filter_input(INPUT_POST, $_POST['name_on_card'], FILTER_SANITIZE_SPECIAL_CHARS);
+  }
+  if (empty($card_number)) {
+    $card_number_err = "Card number is required";
+  } else {
+    $card_number = filter_input(INPUT_POST, $_POST['card_number'], FILTER_SANITIZE_SPECIAL_CHARS);
+  }
+  if (empty($ccv)) {
+    $ccv_err = "CCV is required";
+  } else {
+    $ccv = filter_input(INPUT_POST, $_POST['ccv'], FILTER_SANITIZE_SPECIAL_CHARS);
+  }
+}
+?>
+
 <div class="container-fluid">
   <div class="row">
     <div class="col-sm-6 justify-content-center align-self-center">
@@ -9,45 +58,37 @@
     </div>
     <div class="col-sm-6">
       <div class="col-md-12 order-md-1">
-        <form class="needs-validation" novalidate>
+        <form action="<?php htmlspecialchars($_SERVER['PHP_SELF']); ?>" class="needs-validation" novalidate method="POST">
           <div class="row">
             <div class="col-md-6 mb-3">
               <label for="firstName">Keresztnév</label>
-              <input type="text" class="form-control" id="firstName" placeholder="" value="" required>
-              <div class="invalid-feedback">
-                Adja meg a keresztnevét.
-              </div>
+              <input type="text" class="form-control" id="firstName" name="first_name" placeholder="" required>
+              <div class="invalid-feedback">Adja meg a keresztnevét.</div>
             </div>
             <div class="col-md-6 mb-3">
               <label for="lastName">Vezetéknév</label>
-              <input type="text" class="form-control" id="lastName" placeholder="" value="" required>
-              <div class="invalid-feedback">
-                Adja meg a vezetéknevét.
-              </div>
+              <input type="text" class="form-control" id="lastName" name="last_name" placeholder="" required>
+              <div class="invalid-feedback">Adja meg a vezetéknevét.</div>
             </div>
           </div>
 
           <div class="col--12 mb-3">
             <label for="username">Telefonszám</label>
             <span class="bfh-phone" data-format="+36 (ddd) ddd-dddd" data-number="15555555555"></span>
-            <input type="text" class="form-control" id="bfh-phone" placeholder="Telefonszám" required>
-            <div class="invalid-feedback" style="width: 100%;">
-              Adja meg a telefonszámát.
-            </div>
+            <input type="text" class="form-control" id="bfh-phone" placeholder="Telefonszám" name="cell_number" required>
+            <div class="invalid-feedback" style="width: 100%;">Adja meg a telefonszámát.</div>
 
           </div>
 
           <div class="col--12 mb-3">
             <label for="email">E-mail cím</label>
-            <input type="email" class="form-control" id="email" placeholder="te@pelda.com">
-            <div class="invalid-feedback">
-              Adja meg az e-mail címét.
-            </div>
+            <input type="email" class="form-control" id="email" name="email" placeholder="te@pelda.com">
+            <div class="invalid-feedback">Adja meg az e-mail címét.</div>
           </div>
 
           <div class="col--12 mb-3">
             <label for="s-product">Kiválasztott termék</label>
-            <select class="form-control">
+            <select class="form-control" name="car">
               <option>Porsche 911 Carrera S (2019)</option>
               <option>Porsche 911 Targa 4</option>
               <option>Porsche 911 Turbo Cabriolet</option>
@@ -85,36 +126,28 @@
               <label for="cc-name">Kártyán lévő név</label>
               <input type="text" class="form-control" id="cc-name" placeholder="" required>
               <small class="text-muted">Teljes név a kártyán</small>
-              <div class="invalid-feedback">
-                Írja be a kártyán lévő nevet.
-              </div>
+              <div class="invalid-feedback">Írja be a kártyán lévő nevet.</div>
             </div>
             <div class="col-md-6 mb-3">
               <label for="cc-number">Kártyaszám</label>
               <input type="text" class="form-control" id="cc-number" placeholder="" required>
-              <div class="invalid-feedback">
-                Írja be a kártyaszámot.
-              </div>
+              <div class="invalid-feedback">Írja be a kártyaszámot.</div>
             </div>
           </div>
           <div class="row">
             <div class="col-md-6 mb-3">
               <label for="cc-expiration">Lejárati dátum</label>
               <input type="text" class="form-control" id="cc-expiration" placeholder="" required>
-              <div class="invalid-feedback">
-                Írja be a lejárati dátumot.
-              </div>
+              <div class="invalid-feedback">Írja be a lejárati dátumot.</div>
             </div>
             <div class="col-md-6 mb-3">
               <label for="cc-expiration">Biztonsági kód</label>
               <input type="text" class="form-control" id="cc-cvv" placeholder="" required>
-              <div class="invalid-feedback">
-                Írja be a biztonsági kódot.
-              </div>
+              <div class="invalid-feedback">Írja be a biztonsági kódot.</div>
             </div>
           </div>
           <hr class="mb-3">
-          <button class="btn btn-primary btn-lg btn-block" type="submit" onclick="show_alert();">Vásárlás</button>
+          <button class="btn btn-primary btn-lg btn-block" type="submit" value="Submit" name="submit" onclick="show_alert();">Vásárlás</button>
         </form>
       </div>
     </div>
