843de4573b6d4e77258ccbc7b41109076d0719ec
László Gondi <laszlo.zsolt.gondi@gmail.com>
2022-07-19 15:29:37 +0200

    Modified termekek.php
    It now displays the categories correctly but outputs all the cars for all categories.

--- termekek.php
+++ termekek.php
@@ -4,40 +4,44 @@
 $sql = 'SELECT * FROM cars';
 $result = mysqli_query($connect, $sql);
 $output = mysqli_fetch_all($result, MYSQLI_ASSOC);
+
+$counter = 0;
+$catArr = ['Porsche 911', 'Porsche 718', 'Porsche Panamera', 'Porsche terepjárók'];
 ?>
 
 <div class="container-fluid" style="margin-top:50px">
-    <h3 class="h3">Porsche 911</h3>
-    <div class="row">
-        <?php if (empty($output)) : ?>
-            <p class="mt-3">There are no cars for sale</p>
-        <?php endif; ?>
-        <?php foreach ($output as $item) : ?>
-            <div class="col-md-3 col-sm-6" id="product_card">
-                <div class="product-grid9">
-                    <div class="product-image9">
-                        <a href="#">
-                            <img class="pic-1" src="<?php echo '/images/car' . $item['index']; ?>.jpg">
-                            <ul class="pic-2">
-                                <li><?php echo $item['consumption']; ?> l/100 km</li>
-                                <li><?php echo $item['accel']; ?> </br>Gyorsulás 0 - 100 km/h</li>
-                                <li><?php echo $item['hp']; ?> Le</li>
-                                <li><?php echo $item['topspeed']; ?> Végsebesség</li>
-                                <li><?php echo $item['width']; ?> mm Szélesség</li>
-                                <li><?php echo $item['length']; ?> Hossz</li>
-                            </ul>
-                        </a>
-                    </div>
-                    <div class="product-content">
-                        <h3 class="title"><?php echo $item['name'] ?></h3>
-                        <div class="price"><?php echo $item['price']; ?> EUR</div>
-                        <a class="add-to-cart" href="kapcsolat.php">Vásárlás</a>
-                    </div>
+    <?php for ($i = 0; $i < 4; $i++) : ?>
+        <h3 class="h3"><?php echo $catArr[$i]; ?><h3>
+                <div class="row">
+                    <?php if (empty($output)) : ?>
+                        <p class="mt-3">There are no cars for sale</p>
+                    <?php endif; ?>
+                    <?php foreach ($output as $item) : ?>
+                        <div class="col-md-3 col-sm-6" id="product_card">
+                            <div class="product-grid9">
+                                <div class="product-image9">
+                                    <a href="#">
+                                        <img class="pic-1" src="<?php echo '/images/car' . $item['index']; ?>.jpg">
+                                        <ul class="pic-2">
+                                            <li><?php echo $item['consumption']; ?> l/100 km</li>
+                                            <li><?php echo $item['accel']; ?> </br>Gyorsulás 0 - 100 km/h</li>
+                                            <li><?php echo $item['hp']; ?> Le</li>
+                                            <li><?php echo $item['topspeed']; ?> Végsebesség</li>
+                                            <li><?php echo $item['width']; ?> mm Szélesség</li>
+                                            <li><?php echo $item['length']; ?> Hossz</li>
+                                        </ul>
+                                    </a>
+                                </div>
+                                <div class="product-content">
+                                    <h3 class="title"><?php echo $item['name'] ?></h3>
+                                    <div class="price"><?php echo $item['price']; ?> EUR</div>
+                                    <a class="add-to-cart" href="kapcsolat.php">Vásárlás</a>
+                                </div>
+                            </div>
+                        </div>
+                    <?php endforeach; ?>
+                <?php endfor; ?>
                 </div>
-            </div>
-        <?php endforeach ?>
-
-    </div>
 </div>
 
 <?php include('inc/footer.php'); ?>
