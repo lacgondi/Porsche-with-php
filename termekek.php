f893ebb643da0473f11eb7c79551517a42f68148
László Gondi <laszlo.zsolt.gondi@gmail.com>
2022-07-21 15:14:20 +0200

    Fix for loop displaying the cars for sale in termekek.php

--- termekek.php
+++ termekek.php
@@ -6,47 +6,52 @@ $result = mysqli_query($connect, $sql);
 $output = mysqli_fetch_all($result, MYSQLI_ASSOC);
 
 $counter = 0;
-$catArr = ['Porsche 911', 'Porsche 718', 'Porsche Panamera', 'Porsche terepjárók'];
+$catArr = [
+    1 => 'Porsche 911',
+    2 => 'Porsche 718',
+    3 => 'Porsche Panamera',
+    4 => 'Porsche terepjárók'
+];
 ?>
 
 <div class="container-fluid" style="margin-top:50px">
-    <?php for ($i = 0; $i < 4; $i++) : ?>
-        <h3 class="h3"><?php echo $catArr[$i]; ?><h3>
-                <div class="row">
-                    <?php if (empty($output)) : ?>
-                        <p class="mt-3">There are no cars for sale</p>
+    <?php if (empty($output)) : ?>
+        <p class="mt-3">There are no cars for sale</p>
+    <?php endif; ?>
+    <?php for ($i = 0; $i < count($output); $i++) : ?>
+        <?php if ($i == 0 || $i - 1 != 0 && $i % 4 == 0) : ?>
+            <h3 class="h3"><?php echo $catArr[$output[$i]['type']]; ?><h3>
+                    <div class="row">
                     <?php endif; ?>
-                    <?php foreach ($output as $item) : ?>
-                        <!-- <php if ($item['index'] % 4 == 0 || $item['index'] == 1) : ?>
+                    <!-- <php if ($output[$i]['index'] % 4 == 0 || $output[$i]['index'] == 1) : ?>
                             <div class="row">
-                                <h3 class="h3"><php echo $catArr[$item['index']]; ?><h3>
+                                <h3 class="h3"><php echo $catArr[$output[$i]['index']]; ?><h3>
                             </div>
                         <php endif; ?> -->
-                        <div class="col-md-3 col-sm-6" id="product_card">
-                            <div class="product-grid9">
-                                <div class="product-image9">
-                                    <a href="#">
-                                        <img class="pic-1" src="<?php echo '/images/car' . $item['index']; ?>.jpg">
-                                        <ul class="pic-2">
-                                            <li><?php echo $item['consumption']; ?> l/100 km</li>
-                                            <li><?php echo $item['accel']; ?> </br>Gyorsulás 0 - 100 km/h</li>
-                                            <li><?php echo $item['hp']; ?> Le</li>
-                                            <li><?php echo $item['topspeed']; ?> Végsebesség</li>
-                                            <li><?php echo $item['width']; ?> mm Szélesség</li>
-                                            <li><?php echo $item['length']; ?> Hossz</li>
-                                        </ul>
-                                    </a>
-                                </div>
-                                <div class="product-content">
-                                    <h3 class="title"><?php echo $item['name'] ?></h3>
-                                    <div class="price"><?php echo $item['price']; ?> EUR</div>
-                                    <a class="add-to-cart" href="kapcsolat.php">Vásárlás</a>
-                                </div>
+                    <div class="col-md-3 col-sm-6" id="product_card">
+                        <div class="product-grid9">
+                            <div class="product-image9">
+                                <a href="#">
+                                    <img class="pic-1" src="<?php echo '/images/car' . $output[$i]['index']; ?>.jpg">
+                                    <ul class="pic-2">
+                                        <li><?php echo $output[$i]['consumption']; ?> l/100 km</li>
+                                        <li><?php echo $output[$i]['accel']; ?> </br>Gyorsulás 0 - 100 km/h</li>
+                                        <li><?php echo $output[$i]['hp']; ?> Le</li>
+                                        <li><?php echo $output[$i]['topspeed']; ?> Végsebesség</li>
+                                        <li><?php echo $output[$i]['width']; ?> mm Szélesség</li>
+                                        <li><?php echo $output[$i]['length']; ?> Hossz</li>
+                                    </ul>
+                                </a>
+                            </div>
+                            <div class="product-content">
+                                <h3 class="title"><?php echo $output[$i]['name'] ?></h3>
+                                <div class="price"><?php echo $output[$i]['price']; ?> EUR</div>
+                                <a class="add-to-cart" href="kapcsolat.php">Vásárlás</a>
                             </div>
                         </div>
-                    <?php endforeach; ?>
+                    </div>
                 <?php endfor; ?>
-                </div>
+                    </div>
 </div>
 
 <?php include('inc/footer.php'); ?>
