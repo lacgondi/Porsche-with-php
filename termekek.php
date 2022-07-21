650ed650a82e6361a8065870d19d1b981682a68f
László Gondi <laszlo.zsolt.gondi@gmail.com>
2022-07-21 15:59:19 +0200

    Fix cars displaying in one long column instead of rows of four.

--- termekek.php
+++ termekek.php
@@ -20,33 +20,36 @@ $catArr = [
     <?php endif; ?>
     <?php for ($i = 0; $i < count($output); $i++) : ?>
         <?php if ($i == 0 || $i - 1 != 0 && $i % 4 == 0) : ?>
-            <h3 class="h3"><?php echo $catArr[$output[$i]['type']]; ?><h3>
-                    <div class="row">
-                    <?php endif; ?>
-                    <div class="col-md-3 col-sm-6" id="product_card">
-                        <div class="product-grid9">
-                            <div class="product-image9">
-                                <a href="#">
-                                    <img class="pic-1" src="<?php echo '/images/car' . $output[$i]['index']; ?>.jpg">
-                                    <ul class="pic-2">
-                                        <li><?php echo $output[$i]['consumption']; ?> l/100 km</li>
-                                        <li><?php echo $output[$i]['accel']; ?> </br>Gyorsulás 0 - 100 km/h</li>
-                                        <li><?php echo $output[$i]['hp']; ?> Le</li>
-                                        <li><?php echo $output[$i]['topspeed']; ?> Végsebesség</li>
-                                        <li><?php echo $output[$i]['width']; ?> mm Szélesség</li>
-                                        <li><?php echo $output[$i]['length']; ?> Hossz</li>
-                                    </ul>
-                                </a>
-                            </div>
-                            <div class="product-content">
-                                <h3 class="title"><?php echo $output[$i]['name'] ?></h3>
-                                <div class="price"><?php echo $output[$i]['price']; ?> EUR</div>
-                                <a class="add-to-cart" href="kapcsolat.php">Vásárlás</a>
-                            </div>
-                        </div>
+            <h3 class="h3"><?php echo $catArr[$output[$i]['type']]; ?></h3>
+            <div class="row">
+        <?php endif; ?>
+            <div class="col-md-3 col-sm-6" id="product_card">
+                <div class="product-grid9">
+                    <div class="product-image9">
+                        <a href="#">
+                            <img class="pic-1" src="<?php echo '/images/car' . $output[$i]['index']; ?>.jpg">
+                            <ul class="pic-2">
+                                <li><?php echo $output[$i]['consumption']; ?> l/100 km</li>
+                                <li><?php echo $output[$i]['accel']; ?> </br>Gyorsulás 0 - 100 km/h</li>
+                                <li><?php echo $output[$i]['hp']; ?> Le</li>
+                                <li><?php echo $output[$i]['topspeed']; ?> Végsebesség</li>
+                                <li><?php echo $output[$i]['width']; ?> mm Szélesség</li>
+                                <li><?php echo $output[$i]['length']; ?> Hossz</li>
+                            </ul>
+                        </a>
                     </div>
-                <?php endfor; ?>
+                    <div class="product-content">
+                        <h3 class="title"><?php echo $output[$i]['name'] ?></h3>
+                        <div class="price"><?php echo $output[$i]['price'] ?> EUR</div>
+                        <a class="add-to-cart" href="kapcsolat.php">Vásárlás</a>
                     </div>
+                </div>
+            </div>
+        <?php if ($i % 4 == 3) :  //actual math genius?>
+            </div>
+        <?php endif; ?>
+    <?php endfor; ?>
+
 </div>
 
 <?php include('inc/footer.php'); ?>
