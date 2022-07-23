6d77723bac54b53962005354031e11e23d329467
László Gondi <laszlo.zsolt.gondi@gmail.com>
2022-07-23 15:44:50 +0200

    Add JS code to trigger the popup.

--- popup.php
+++ popup.php
@@ -1,9 +1,11 @@
+<?php include('inc/header.php'); ?>
+
 <div class="modal fade" id="orderConfirm" tabindex="-1" role="dialog" aria-labelledby="orderConfirmLabel" aria-hidden="true">
     <div class="modal-dialog modal-dialog-centered" role="document">
         <div class="modal-content">
             <div class="modal-header">
                 <h5 class="modal-title" id="orderConfirmLabel">Order successful</h5>
-                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close" id="home">
                     <span aria-hidden="true">&times;</span>
                 </button>
             </div>
@@ -11,7 +13,7 @@
                 Your order is successful. We will get back to you with the paperwork shortly.
             </div>
             <div class="modal-footer">
-                <button type="button" class="btn btn-primary" data-dismiss="modal">OK</button>
+                <button type="button" class="btn btn-primary" data-dismiss="modal" id="home">OK</button>
             </div>
         </div>
     </div>
@@ -19,3 +21,12 @@
 </div>
 </div>
 </div>
+
+<script type="text/javascript">
+    let myModal = new bootstrap.Modal(document.getElementById('orderConfirm'), {});
+    myModal.show();
+
+    document.getElementById("home").onclick = function() {
+        window.location.replace("index.php");
+    };
+</script>
