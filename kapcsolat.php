be0b25faa291e7cfaf6a7978efb28d630d09c62f
László Gondi <laszlo.zsolt.gondi@gmail.com>
2022-07-23 14:50:07 +0200

    Add pop-up element.

--- kapcsolat.php
+++ kapcsolat.php
@@ -143,8 +143,28 @@ if (isset($_POST['submit'])) {
             </div>
           </div>
           <hr class="mb-3">
-          <button class="btn btn-primary btn-lg btn-block" type="submit" onclick="show_alert();">Vásárlás</button>
+          <button class="btn btn-primary btn-lg btn-block" type="submit" onclick="">Vásárlás</button>
         </form>
+      </div>
+    </div>
+  </div>
+
+  <!-- modal -->
+  <div class="modal fade" id="orderConfirm" tabindex="-1" role="dialog" aria-labelledby="orderConfirmLabel" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered" role="document">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title" id="orderConfirmLabel">Order successful</h5>
+          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+          </button>
+        </div>
+        <div class="modal-body">
+          Your order is successful. We will get back to you with the paperwork shortly.
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-primary">OK</button>
+        </div>
       </div>
     </div>
   </div>
